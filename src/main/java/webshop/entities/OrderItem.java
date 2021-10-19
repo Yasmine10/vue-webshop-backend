62319a850e0925d6694e75eb45c06c8d2c54c6b4
Yasmine De Wolf <yasmine.de.wolf@student.ehb.be>
2021-10-19 12:32:39 +0200

    added order and orderItem entities

---
+++ src/main/java/webshop/entities/OrderItem.java
@@ -0,0 +1,57 @@
+package webshop.entities;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Table(name = "orderItems")
+@Entity
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private int quantity;
+
+    @OneToOne
+    @JoinColumn(name = "product_id", referencedColumnName = "id")
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id", referencedColumnName = "id")
+    private Order order;
+
+    public OrderItem() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+}
